dc73a20f4557cd9bc0d182f8516063962001391a
David Mehren <git@herrmehren.de>
2022-02-13 17:48:38 +0100

    feat(note): add version attribute
    This attribute was defined in the dev docs,
    but never implemented.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/notes/note-metadata.dto.ts
+++ src/notes/note-metadata.dto.ts
@@ -66,6 +66,10 @@ export class NoteMetadataDto {
   @ApiProperty()
   tags: string[];
 
+  @IsNumber()
+  @ApiProperty()
+  version: number;
+
   /**
    * Datestring of the last time this note was updated
    * @example "2020-12-01 12:23:34"
